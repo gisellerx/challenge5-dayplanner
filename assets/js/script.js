b19a58c969fd84782cc53d8c5096339ea4c28f74
gisellerx <giselle.armas@outlook.com>
2023-09-21 17:29:41 -0400

    added for loop to js for time tracking

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,12 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+
+  for (let i = 9; i < 17; i++) {
+    const element = array[i];
+    
+  }
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
